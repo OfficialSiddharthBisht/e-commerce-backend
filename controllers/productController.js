4857808ba92d572e22724b850cbb16d3c1df9f3c
Siddharth <siddharth@flamecloud.co.uk>
2022-10-19 23:22:50 +0530

    ADD: Update a product controller added

--- controllers/productController.js
+++ controllers/productController.js
@@ -19,3 +19,22 @@ exports.getAllProducts = async(req, res) =>{
     });
 }
 
+// Update a product ---admin route
+exports.updateProduct = async(req,res,next) =>{
+    let product = Product.findById("req.params.id");
+    if(!product){
+        return res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new: true, 
+        runValidators:true,
+        useFFindAndModify:false
+    });
+    res.status(200).json({
+        success : true,
+        product
+    })
+}
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { getAllProducts,createProduct } = require("../controllers/productController");
+const { getAllProducts,createProduct, updateProduct } = require("../controllers/productController");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
-
+router.route("/product/:id").put(updateProduct);
 
 module.exports = router;
