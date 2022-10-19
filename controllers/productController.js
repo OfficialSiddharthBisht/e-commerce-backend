98008da1bba5e540cb79255dd8b2b6a3094fdd4f
Siddharth <siddharth@flamecloud.co.uk>
2022-10-19 21:55:57 +0530

    ADD: product controller -> create product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,15 @@
+const Product = require("../models/productModels");
+
+
+// Create Product 
+exports.createProduct = async(req, res, next) =>{
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
 
 exports.getAllProducts = (req, res) =>{
     res.status(200).json({message : "Route is working fine"});
