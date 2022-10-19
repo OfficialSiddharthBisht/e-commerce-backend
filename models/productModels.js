6770f979e81f9f22d205ca3443ea92cefe472daf
Siddharth <siddharth@flamecloud.co.uk>
2022-10-19 21:52:04 +0530

    ADD: Product Model

---
+++ models/productModels.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter product name"],
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: [true, "Please enter product description"],
+    },
+    price: {
+        type: Number,
+        required: [true, "Please enter product price"],
+        maxLength: [12, "Price cannot exceed 100 Trillion"],
+    },
+    ratings: {
+        type: Number,
+        default: 0
+    },
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true,
+            }
+        }
+    ],
+    categories : {
+        type : String,
+        required : [true, "Please enter product category"],
+    },
+    stock : {
+        type : Number,
+        required : [true, "Please enter product stock"],
+        maxLength: [5, "Product stock cannot exceed ten thousand"],
+        default : 0,
+    },
+    numOfReviews : {
+        type : Number,
+        default : 0,
+    },
+    reviews : [
+        {
+            name : {
+                type : String,
+                required: true,
+            },
+            rating :{
+                type : Number,
+                required :true,
+            },
+            comment: {
+                type: String,
+            }
+        }
+    ],
+    createdAt : {
+        type :Date,
+        default : Date.now,
+    }
+})
+
+module.exports = mongoose.model("Product",productSchema);
