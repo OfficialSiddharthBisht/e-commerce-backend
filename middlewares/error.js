e2ff7b9f57a09f8846da6c98246690dd0454af19
Siddharth <officialsiddharthbisht@gmail.com>
2022-10-28 17:36:31 +0530

    PREVENT: Cast error

--- middlewares/error.js
+++ middlewares/error.js
@@ -5,6 +5,13 @@ module.exports = (err , req , res ,next) =>{
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server Error";
 
+    // cast error
+    // Wrong mongodb id error
+    if(err.name === "CaseError"){
+        const message = `Resource not found. Invalid : ${err.path}`;
+        err = new ErrorHandler(message,400);
+    }
+
     res.status(err.statusCode).json({
         success : false,
         message: err.message,
