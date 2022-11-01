82a2f0f7901e88061232ecbfe3ecac230a479a18
Siddharth <officialsiddharthbisht@gmail.com>
2022-11-01 08:59:06 +0530

    ERROR-HANDLING: wrong jwt token and token expire

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,25 +1,37 @@
 const ErrorHandler = require("../utils/errorHandler");
 
 
-module.exports = (err , req , res ,next) =>{
+module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server Error";
 
     // cast error
     // Wrong mongodb id error
-    if(err.name === "CaseError"){
+    if (err.name === "CaseError") {
         const message = `Resource not found. Invalid : ${err.path}`;
-        err = new ErrorHandler(message,400);
+        err = new ErrorHandler(message, 400);
     }
 
     // Mongoose Dublicate key error
-    if(err.code === 11000){
+    if (err.code === 11000) {
         const message = `Dublicate ${Object.keys(err.keyValue)} Entered`
-        err = new ErrorHandler(message,400);
+        err = new ErrorHandler(message, 400);
+    }
+
+    // Wrong JWT token error
+    if (err.name === "JsonWebTokenError") {
+        const message = `Json Web Token is invalid, try again`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    // JWT Expire error
+    if (err.name === "TokenExpiredError") {
+        const message = `Json Web Token is expired, try again`;
+        err = new ErrorHandler(message, 400);
     }
 
     res.status(err.statusCode).json({
-        success : false,
+        success: false,
         message: err.message,
         // error : err.stack 
         // for exact error (detailed one)
