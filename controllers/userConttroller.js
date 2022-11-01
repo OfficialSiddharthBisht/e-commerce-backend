71692088f0f0f13f6a5f01d12b40d9a6a18104ed
Siddharth <officialsiddharthbisht@gmail.com>
2022-11-01 09:06:00 +0530

    ADD:Get user details route

--- controllers/userConttroller.js
+++ controllers/userConttroller.js
@@ -133,3 +133,13 @@ exports.resetPassword = catchAsyncErrors(async(req, res,next)=>{
 
         sendToken(user,200,res);
 })
+
+// Get user details -> works only after login 
+exports.getUserDetails = catchAsyncErrors(async (req,res,next)=>{
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        user,
+    })  
+})
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { registerUser, loginUser,logout, forgotPassword, resetPassword } = require("../controllers/userConttroller");
+const { registerUser, loginUser,logout, forgotPassword, resetPassword, getUserDetails } = require("../controllers/userConttroller");
+const { isAuthenticatedUser,authorizeRoles } = require("../middlewares/auth");
 const router = express.Router();
 
 router.route("/register").post(registerUser);
@@ -7,5 +8,6 @@ router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").post(logout);
+router.route("/me").get(isAuthenticatedUser,getUserDetails);
 module.exports = router;
 
