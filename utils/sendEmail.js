8f113e4824d7daf2a02eed94c19073d07b74a0b5
Siddharth <officialsiddharthbisht@gmail.com>
2022-11-01 08:39:39 +0530

    ADD: email password reset

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,8 +1,11 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async(options) =>{
-    const transporter = nodeMailer.createTransport({
+    const transporter = nodemailer.createTransport({
+        host : "smtp.gmail.com",
+        port: 465,
         service: process.env.SMTP_SERVICE,
+        secure: true,
         auth: {
             user :process.env.SMTP_MAIL,
             pass: process.env.SMTP_PASSWORD,
